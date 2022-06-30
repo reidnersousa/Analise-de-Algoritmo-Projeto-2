074951bbd6a30f098ed47074cd192509ba07d2ee
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2022-06-30 12:20:36 -0300

    usando for e  if
    foi criado uma função com for para percorrer  a lista de itens_otimos . Esse for percorrer essa lista com base no tamanho da lista que pode ser definido utilizando a  variavel n que nos da o tamanho da lista

--- dataset/Amain.py
+++ dataset/Amain.py
@@ -1,3 +1,6 @@
+from pickle import TRUE
+
+
 f = open("large_scale/knapPI_1_200_1000_1", "r")
 
 primeira_linha = f.readline()
@@ -25,9 +28,23 @@ ultima_linha = f.readline()
 
 itens_otimos = [bool(int(i)) for i in ultima_linha.split()]
 
-print(itens_otimos) 
+print(">",itens_otimos) 
 
 
 
 ## teste  o que tive daqui para baixo pode ser considerado erro 
 print("<",valores[0],valores[1])
+
+print(type(itens_otimos[0]))
+
+carro =True
+if carro ==True:
+    print("Bum")
+
+# função que vai pecorrer a lista de itens_otimos
+for i in range(int(n)):
+    
+   # print(itens_otimos[i])
+    if itens_otimos[i]==True:
+        print("VVV",[i])#16 no total são True
+        
