02fa4bb61618298a0d1636237218a78b226e0478
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2022-06-30 12:51:24 -0300

    Pesos otimos e valores otimos
    Usando um if foi possivel colocar os valores dentro de uma lista de pesos otimos e valores otimos

--- dataset/Amain.py
+++ dataset/Amain.py
@@ -9,7 +9,8 @@ n, w = primeira_linha.split()
 
 pesos = []
 valores = []
-
+pesosOtimos =[]
+valoresOtimos =[]
 for i in range(int(n)):
     linha = f.readline()
 
@@ -28,11 +29,14 @@ ultima_linha = f.readline()
 
 itens_otimos = [bool(int(i)) for i in ultima_linha.split()]
 
-print(">",itens_otimos) 
+# print(">",itens_otimos) imprimir os boolean  
 
 
 
-## teste  o que tive daqui para baixo pode ser considerado erro 
+
+
+
+## tudo certo . ultimo commit feito 12:00 30/06/2022
 print("<",valores[0],valores[1])
 
 print(type(itens_otimos[0]))
@@ -41,10 +45,21 @@ carro =True
 if carro ==True:
     print("Bum")
 
-# função que vai pecorrer a lista de itens_otimos
+
+
+## segunda implementação tudo certo .O algorimto atende o que foi requisitado .
 for i in range(int(n)):
     
    # print(itens_otimos[i])
     if itens_otimos[i]==True:
-        print("VVV",[i])#16 no total são True
-        
+        #print("VVV",[i])#16 no total com true . verificando se o elemento foi inserido no if
+        pesosOtimos.append(pesos[i])
+        valoresOtimos.append(valores[i])
+
+
+print("Imprimindos os pesos dos valores que foram inseridos na mochila\n ",pesosOtimos)
+print("Imprimindos os valores que foram inserido na mochila\n",valoresOtimos)
+
+## terceira implementação .Periodo de teste 
+##  objetivo fazer a soma dos valores dos pesosOtimos e valoresOtimos 
+
